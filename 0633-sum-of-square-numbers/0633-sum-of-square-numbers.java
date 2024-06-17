a7fb0156cc5635bcc97abe5b9987fe388afc2b98
MonikaSomasundaram <165294230+MonikaSomasundaram@users.noreply.github.com>
2024-06-17 14:26:05 +0530

    Time: 3 ms (97.79%), Space: 40.2 MB (50.69%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -4,9 +4,7 @@ class Solution {
      long  l=(long)Math.sqrt(c);
      while(f<=l){
         long  t=f*f+l*l;
-        long  t1=f*f+f*f;
-        long  t2=l*l+l*l;
-        if(t==c || t1==c || t2==c){
+        if(t==c){
             return true;
         }
         else if(t>c){
