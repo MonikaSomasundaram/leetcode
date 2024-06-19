17e6ae3e5c040dcf98448d8d0010d56cad76987e
MonikaSomasundaram <165294230+MonikaSomasundaram@users.noreply.github.com>
2024-06-19 15:30:10 +0530

    Time: 1 ms (70.06%), Space: 46.5 MB (12.61%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,26 +1,21 @@
 class Solution {
     public int trap(int[] height) {
-        int pre[]=new int[height.length];
-        int suf[]=new int[height.length];
-        pre[0]=height[0];
-        for(int i=1;i<height.length;i++){
-            if(height[i]>pre[i-1])
-                pre[i]=height[i];
-            else
-                pre[i]=pre[i-1];
-        }
-        suf[height.length-1]=height[height.length-1];
-        for(int i=height.length-2;i>=0;i--){
-            if(height[i]>suf[i+1])
-                suf[i]=height[i];
-            else
-                suf[i]=suf[i+1];
-        }
-        int min;
+        int l=0;
+        int r=height.length-1;
+        int lmax=0;
+        int rmax=0;
         int ans=0;
-        for(int i=0;i<height.length;i++){
-           min=pre[i]<suf[i]?pre[i]:suf[i];
-           ans+=min-height[i]; 
+        while(l<=r){
+            if(height[l]<=height[r]){
+                if(height[l]>=lmax) lmax=height[l];
+                else ans+=lmax-height[l];
+                l++;
+            }
+            else{
+                if(height[r]>=rmax) rmax=height[r];
+                else ans+=rmax-height[r];
+                r--;
+            }
         }
         return ans;
     }
