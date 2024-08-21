5e06583609274d71702cf2ec42149e08babecb17
MonikaSomasundaram <210701166@rajalakshmi.edu.in>
2024-08-21 21:13:35 +0530

    Time: 12 ms (27.59%), Space: 45.5 MB (6.1%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -4,7 +4,7 @@ class Solution {
         String ans="";
         String arr[]=s.split("\\s+");
         for(int i=arr.length-1;i>=0;i--){
-            System.out.println(arr[i]);
+            //System.out.println(arr[i]);
             ans+=arr[i].trim()+" ";
         }
         return ans.trim();
