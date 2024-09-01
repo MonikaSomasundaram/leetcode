805cf945ddc5518cc842cf878558fb2daad49cb2
MonikaSomasundaram <210701166@rajalakshmi.edu.in>
2024-09-01 20:17:32 +0530

    Time: 4 ms (46.66%), Space: 44.1 MB (69.98%) - LeetHub

--- 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.java
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.java
@@ -1,18 +1,8 @@
 class Solution {
     public boolean canBeEqual(int[] target, int[] arr) {
-        HashMap<Integer,Integer> hm=new HashMap<>();
-        for(int i:arr){
-          hm.put(i,hm.getOrDefault(i,0)+1);
-        }
-        for(int i:target){
-            if(!hm.containsKey(i))return false;
-            else{
-                hm.put(i,hm.get(i)-1);
-            }
-        }
-        for(int i:hm.keySet()){
-            if(hm.get(i)!=0) return false;
-        }
-        return true;
+        Arrays.sort(target);
+        Arrays.sort(arr);
+        if(Arrays.equals(target,arr))return true;
+        return false;
     }
 }
